53bca338925553aa4e13dc6c024f06801b894921
FrankOliveira422 <135998129+FrankOliveira422@users.noreply.github.com>
2024-07-03 23:14:05 -0400

    Update app.py

--- app.py
+++ app.py
@@ -2,8 +2,35 @@ from flask import Flask, request, render_template, redirect, url_for
 import pytube
 import os
 import time
+import urllib
 
 app = Flask(__name__)
+
+def download_video(url, format_type, retries=3):
+    while retries > 0:
+        try:
+            video = pytube.YouTube(url)
+            if format_type == 'mp4':
+                stream = video.streams.get_highest_resolution()
+                filename = f"{video.title}.mp4"
+            elif format_type == 'mp3':
+                stream = video.streams.filter(only_audio=True).first()
+                filename = f"{video.title}.mp3"
+            else:
+                return "Formato inválido!"
+
+            download_path = os.path.join(os.path.expanduser("~"), "Downloads")
+            if not os.path.exists(download_path):
+                os.makedirs(download_path)
+            stream.download(output_path=download_path, filename=filename)
+            return None
+        except urllib.error.HTTPError as e:
+            if e.code == 429:
+                time.sleep(5)  # Aumentar o tempo de espera em cada retentativa
+                retries -= 1
+            else:
+                return f"Ocorreu um erro ao baixar o vídeo: {str(e)}"
+    return "Limite de retentativas atingido."
 
 @app.route('/')
 def index():
@@ -12,33 +39,10 @@ def index():
 @app.route('/download', methods=['POST'])
 def download():
     url = request.form['url']
-    try:
-        video = pytube.YouTube(url)
-        time.sleep(2)  # Adicionar uma pausa de 2 segundos entre requisições
-
-        format_type = request.form['format']
-
-        if format_type == 'mp4':
-            stream = video.streams.get_highest_resolution()
-            filename = f"{video.title}.mp4"
-        elif format_type == 'mp3':
-            stream = video.streams.filter(only_audio=True).first()
-            filename = f"{video.title}.mp3"
-        else:
-            return "Formato inválido!"
-
-        download_path = os.path.join(os.path.expanduser("~"), "Downloads")
-        if not os.path.exists(download_path):
-            os.makedirs(download_path)
-        stream.download(output_path=download_path, filename=filename)
-        
-    except pytube.exceptions.RegexMatchError as e:
-        return f"Erro ao processar a URL: {str(e)}"
-    except pytube.exceptions.VideoUnavailable:
-        return "Vídeo não disponível."
-    except Exception as e:
-        return f"Ocorreu um erro ao baixar o vídeo: {str(e)}"
-
+    format_type = request.form['format']
+    error_message = download_video(url, format_type)
+    if error_message:
+        return error_message
     return redirect(url_for('index'))
 
 if __name__ == '__main__':
