446589306fc4e2f416dc3a43c266b253f9eb9c8a
FrankOliveira422 <135998129+FrankOliveira422@users.noreply.github.com>
2024-07-04 08:58:41 -0400

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask, request, render_template, redirect, url_for
 import pytube
 import os
+import time
 
 app = Flask(__name__)
 
@@ -10,26 +11,28 @@ def index():
 
 @app.route('/download', methods=['POST'])
 def download():
-    url = request.form['url']
-    video = pytube.YouTube(url)
+    try:
+        url = request.form['url']
+        video = pytube.YouTube(url)
+        time.sleep(2)  # Espera de 2 segundos para reduzir o risco de sobrecarga
 
-    # Escolher o formato do arquivo (MP4 ou MP3)
-    format_type = request.form['format']
+        format_type = request.form['format']
+        if format_type == 'mp4':
+            stream = video.streams.get_highest_resolution()
+            filename = f"{video.title}.mp4"
+        elif format_type == 'mp3':
+            stream = video.streams.filter(only_audio=True).first()
+            filename = f"{video.title}.mp3"
+        else:
+            return "Formato inválido!"
 
-    if format_type == 'mp4':
-        stream = video.streams.get_highest_resolution()
-        filename = f"{video.title}.mp4"
-    elif format_type == 'mp3':
-        stream = video.streams.filter(only_audio=True).first()
-        filename = f"{video.title}.mp3"
-    else:
-        return "Formato inválido!"
+        download_path = os.path.join(os.path.expanduser('~'), 'Downloads')
+        stream.download(output_path=download_path, filename=filename)
 
-    # Obter o caminho da pasta de downloads do usuário
-    download_path = os.path.join(os.path.expanduser('~'), 'Downloads')
-
-    # Realizar o download
-    stream.download(output_path=download_path, filename=filename)
+    except pytube.exceptions.PytubeError as e:
+        return f"Erro ao baixar o vídeo: {e}"
+    except Exception as e:
+        return f"Erro inesperado: {e}"
 
     return redirect(url_for('index'))
 
