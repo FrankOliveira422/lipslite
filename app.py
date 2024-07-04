8d259a22e4fab87d8dcb830f167a318250708568
FrankOliveira422 <135998129+FrankOliveira422@users.noreply.github.com>
2024-07-04 09:11:49 -0400

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,15 @@
-from flask import Flask, request, render_template, redirect, url_for
+from flask import Flask, request, render_template, redirect, url_for, flash
 from pytube import YouTube
 import os
 import time
 import urllib.error
+import logging
 
 app = Flask(__name__)
+app.secret_key = 'secret_key_for_session'  # Necessário para usar flash messages
+
+# Configurar logs
+logging.basicConfig(level=logging.INFO)
 
 def download_video(url, format_type, max_retries=3):
     retry_delay = 5  # Segundos de espera inicial entre retentativas
@@ -28,18 +33,21 @@ def download_video(url, format_type, max_retries=3):
             return None
         except urllib.error.HTTPError as e:
             if e.code == 429:
+                logging.warning(f"Erro 429: Tentativa {retries + 1}. Esperando {retry_delay} segundos.")
                 if retries < max_retries - 1:
-                    print(f"Retentativa {retries + 1} após {retry_delay} segundos.")
                     time.sleep(retry_delay)
                     retry_delay *= 2  # Aumenta o tempo de espera exponencialmente
                     retries += 1
                 else:
-                    return "Limite de retentativas atingido."
+                    logging.error("Limite de retentativas atingido.")
+                    return "Limite de retentativas atingido. Tente novamente mais tarde."
             else:
+                logging.error(f"Erro HTTP {e.code}: {str(e)}")
                 return f"Erro HTTP {e.code}: {str(e)}"
         except Exception as e:
+            logging.error(f"Ocorreu um erro ao baixar o vídeo: {str(e)}")
             return f"Ocorreu um erro ao baixar o vídeo: {str(e)}"
-    return "Limite de retentativas atingido."
+    return "Limite de retentativas atingido. Tente novamente mais tarde."
 
 @app.route('/')
 def index():
@@ -51,7 +59,7 @@ def download():
     format_type = request.form['format']
     error_message = download_video(url, format_type)
     if error_message:
-        return error_message
+        flash(error_message)
     return redirect(url_for('index'))
 
 if __name__ == '__main__':
